a436994a6d2d95f0141818ec9a3948826a2b7543
blessedjasonmwanza <mwanzabj@gmail.com>
2021-11-03 12:45:49 +0200

    Add text range logic to limit the number of characters in a string

--- string-counter.js
+++ string-counter.js
@@ -1,5 +1,10 @@
 const stringLength = (string) => {
-    return string.length;
+    const strLen = string.length;
+    if(strLen >= 1 && strLen<= 10) {
+        return strLen;
+    } else {
+        throw new Error('String length must be between 1-10');
+    }
 }
 
 module.exports =  stringLength;
