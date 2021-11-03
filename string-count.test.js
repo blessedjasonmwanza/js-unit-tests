529c5795ea34f432ed55264e6bf30f5b22214f45
blessedjasonmwanza <mwanzabj@gmail.com>
2021-11-03 12:46:52 +0200

    improve code quality by wrapping test login in one block

--- string-count.test.js
+++ string-count.test.js
@@ -2,9 +2,6 @@ const strCounter = require('./string-counter.js');
 
 test("takes a text as paramenter and returns its lenth", () => {
     expect(strCounter("blessed")).toBe(7);
-});
-
-test("should throw an error if text not between between or equal to 1-10", () => {
     expect(() => {
         strCounter("Hello, My names are blessed Jason");
     }).toThrow();
