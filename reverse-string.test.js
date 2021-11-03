094bac34e31f70110ea2df34ab85c2c85ff3c8aa
blessedjasonmwanza <mwanzabj@gmail.com>
2021-11-03 13:09:40 +0200

    add test extra test case for reversing string

--- reverse-string.test.js
+++ reverse-string.test.js
@@ -2,4 +2,5 @@ const reverse = require('./reverse-string.js');
 
 test("Should pass when string is reversed", () => {
     expect(reverse('blessed')).toMatch('desselb');
+    expect(reverse('blessed')).not.toMatch('bessel');
 });
